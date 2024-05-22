9feb09dfc885cf257ae124476fb5b698ba83cb87
nathanl-dwl <139763838+nathanl-dwl@users.noreply.github.com>
2024-05-22 11:59:45 +0100

    Add files via upload

---
+++ App_Plugins/WonderSEOChecker/UUI/uui-imports.js
@@ -0,0 +1,7 @@
+import '@umbraco-ui/uui-button';
+import '@umbraco-ui/uui-loader-circle';
+import '@umbraco-ui/uui-popover-container';
+import '@umbraco-ui/uui-tag';
+import '@umbraco-ui/uui-box';
+import '@umbraco-ui/uui-menu-item';
+import '@umbraco-ui/uui-popover-container';
