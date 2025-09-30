68227e03aec7ef26dba3120cd91830ac24975f10
Nathan Love <nathanl@digitalwonderlab.com>
2025-09-30 10:46:42 +0100

    changes

--- App_Plugins/WonderSEOChecker/wondercheck.controller.js
+++ App_Plugins/WonderSEOChecker/wondercheck.controller.js
@@ -1,19 +1,16 @@
 angular.module("umbraco").factory("UrlService", function (editorState, $q) {
   return {
-      getCurrentUrl: function () {
-        //async promise for fetching the url related to the back-office page
-          var deferred = $q.defer();
-      //retrieving the back-office page's node and all urls associated with it
+    getCurrentUrl: function () {
+      var deferred = $q.defer();
       var currentNode = editorState.getCurrent();
       var allUrls = currentNode.urls;
-      //checking if any urls are available and initialising url variable if so
+
       if (allUrls && allUrls.length > 0) {
         var url = "";
-        //if there is only one url, it's text value is assigned to the variable
+
         if (allUrls.length === 1) {
           url = allUrls[0].text;
         } else {
-            //checks if there are multiple regions for urls, assigns a matchingUrl variable if the back-office node and url culture are a match, the matching url then becomes the url variable
           var culture = currentNode.culture;
           var matchingUrl = allUrls.find(function (url) {
             return url.culture === culture;
@@ -25,17 +22,17 @@ angular.module("umbraco").factory("UrlService", function (editorState, $q) {
             deferred.reject("Matching URL not found.");
           }
         }
-        //appending the relative url with the location protocol and host to create an absolute url for the api to analyse
+
         if (!url.startsWith("http://") && !url.startsWith("https://")) {
           url = window.location.protocol + "//" + window.location.host + url;
         }
-        //resolving the promise and passing the successfully constructed url variable
+
         deferred.resolve(url);
       } else {
         console.error("No URLs found.");
         deferred.reject("No URLs found.");
       }
-      //returning the promise object for use of .then in the next function
+
       return deferred.promise;
     },
   };
@@ -45,23 +42,18 @@ angular
   .module("umbraco")
   .factory("PageSpeedService", function ($http, editorState, $q, UrlService) {
     return {
-        runPageSpeedTest: function () {
-          //defining the lighthouse api
+      runPageSpeedTest: function () {
         const api =
           "https://www.googleapis.com/pagespeedonline/v5/runPagespeed";
 
-          //running the previous function to obtain the url variable
-            return UrlService.getCurrentUrl().then(function (currentUrl) {
-            //$http provides HTTP methods get, post, put etc.
-                return $http
-          //defining the url to be analysed and setting the categories
+        return UrlService.getCurrentUrl().then(function (currentUrl) {
+          return $http
             .get(api, {
               params: {
                 url: currentUrl,
                 category: ["seo", "performance"],
               },
             })
-            //returning the response in json format
             .then(function (response) {
               return response.data;
             })
@@ -83,21 +75,22 @@ angular
       vm.metaDescription = "";
       vm.pageTitle = "";
       vm.pageError = false;
-      //function to be called on click
+
       vm.runPageSpeed = function ($event) {
         $event.preventDefault();
         vm.loading = true;
 
         vm.pageSpeedResult = null;
         vm.errorMessage = null;
-        //earlier function is called and values are assigned to the vm variable to allow for use in the .html angular file
+
         PageSpeedService.runPageSpeedTest()
           .then(function (pageSpeedResult) {
             vm.pageSpeedResult = pageSpeedResult;
             vm.parsedSpeedIndex = parseFloat(
               pageSpeedResult.lighthouseResult.audits["speed-index"]
                 .displayValue
-              );
+            );
+
             vm.loading = false;
           })
           .catch(function (error) {
@@ -108,7 +101,7 @@ angular
 
           });
       };
-      //fetches the current url for display on the front end (currently not in use)
+
       vm.fetchCurrentUrl = function () {
         UrlService.getCurrentUrl()
           .then(function (url) {
@@ -125,10 +118,9 @@ angular
         $http
             .get(url)
             .then(function (response) {
-                //parser created to view response data from url variable as a text/html doc
                 var parser = new DOMParser();
                 var doc = parser.parseFromString(response.data, "text/html");
-                //meta description and title selected from the parsed doc, truncated version of metadescription created for web preview section
+    
                 var metaDescriptionTag = doc.querySelector(
                     "meta[name='description']"
                 );
@@ -142,7 +134,7 @@ angular
                 if (titleTag) {
                     vm.pageTitle = titleTag.innerText;
                 } 
-                //ensure these variables are correctly updated for use in UI outside of function scope
+    
                 $scope.$apply();
             })
             .catch(function (error) {
